4221d10454b09d8c43d29fe92a750c204987ab82
Fabricio C Zuardi <fabricio@fabricio.org>
2017-07-26 23:42:34 -0300

    basic worker

--- src/sw.js
+++ src/sw.js
@@ -1,2 +1,17 @@
 // @flow
-console.log("foo");
+
+const onInstall = event => {
+  console.log({ event });
+};
+
+const onFetch = event => {
+  console.log({ event });
+  console.log(event.request.url);
+};
+
+const onActivate = event => {
+  console.log({ event });
+};
+self.addEventListener("install", onInstall);
+self.addEventListener("fetch", onFetch);
+self.addEventListener("activate", onActivate);
