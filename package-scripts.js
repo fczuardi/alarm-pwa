1385248d3fc73c818e909e9058013a72148e6c42
Fabricio C Zuardi <fabricio@fabricio.org>
2017-07-27 15:36:06 -0300

    moving npm scripts to a js file for more readability and flexibility

---
+++ package-scripts.js
@@ -0,0 +1,33 @@
+const { concurrent } = require("nps-utils")
+
+const vendors = ["choo", "choo/html", "choo-log"];
+const transforms = ["unflowify", "es2040"].map(t => "transform " + t);
+const appArgs = [
+  "entry src/app.js",
+  "outfile docs/app.js",
+  "no-bundle-external",
+  ...transforms
+];
+const swArgs = [
+  "entry src/sw.js",
+  "outfile docs/sw.js",
+  "no-bundle-external",
+  ...transforms
+];
+
+module.exports = {
+  scripts: {
+    default: "budo --dir src src/app.js",
+    bundle: {
+      app: "browserify " + ["", ...appArgs].join(" --"),
+      sw: "browserify " + ["", ...swArgs].join(" --"),
+      vendors:
+        "browserify " +
+        ["", ...vendors].join(" -r ") +
+        " --outfile docs/vendors.js",
+      default: concurrent.nps("bundle.app", "bundle.sw", "bundle.vendors")
+    },
+    fmt: "prettier --write",
+    precommit: "lint-staged"
+  }
+};
